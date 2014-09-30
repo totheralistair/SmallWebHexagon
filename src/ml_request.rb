fbf4b5d492e0871ceb732e9dd3cbffdc76f18f7d
Alistair <totheralistair@aol.com>
2014-09-30 09:24:17 -0700

    tiny tidying.

--- src/ml_request.rb
+++ src/ml_request.rb
@@ -18,10 +18,6 @@ class Ml_RackRequest < Ml_request
     @myRequest = rack_request
   end
 
-  def theParams ;  @myRequest.params ; end
-  def thePath ;  @myRequest.path ; end
-
-
   def get?; @myRequest.get? ;  end
   def post?; @myRequest.post? || thePath=="/post"            ; end
   def add?;  theParams.has_key?("Add")    ; end
@@ -33,20 +29,21 @@ class Ml_RackRequest < Ml_request
     end
   end
 
+  def theParams ;  @myRequest.params ; end
+  def thePath ;  @myRequest.path ; end
 
   def name_from_path ;  thePath[ 1..thePath.size ] ;  end
   def id_from_path ;  id_from_name( name_from_path )     ;  end
-  def incoming_muffin_name;  theParams["MuffinNumber"]   ;  end
-  def incoming_muffin_id; n = incoming_muffin_name ; id_from_name( n ) ;  end
-  def incoming_contents;  theParams["MuffinContents"] ;  end
-
-
-
   def id_from_name( name ) ;  number_or_nil(name) ;  end
   def number_or_nil( s ) # convert string to a number, nil if not a number
     i= s.to_i
     i.to_s == s ? i : nil
   end
+
+  def incoming_muffin_name;  theParams["MuffinNumber"]   ;  end
+  def incoming_muffin_id; n = incoming_muffin_name ; id_from_name( n ) ;  end
+  def incoming_contents;  theParams["MuffinContents"] ;  end
+
 
 end
 
