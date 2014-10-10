fad21ab110977c288ee9d49d71ba840a8c598b32
Alistair <totheralistair@aol.com>
2014-10-10 15:30:15 -0400

    For Pat: imho, printing the contents of a public read accessor should not cause serialization result to change.

---
+++ test/test_for_Pat.rb
@@ -0,0 +1,62 @@
+require 'rack/test'
+require 'rspec/expectations'
+require 'test/unit'
+require 'erubis'
+require 'fileutils'
+require 'yaml'
+require_relative '../src/for_Pat_ml_request_minus_params_read.rb'
+Test::Unit::TestCase.include RSpec::Matchers
+
+
+class TestRequests < Test::Unit::TestCase
+
+  def new_ml_request method, path, params={}
+    Ml_RackRequest.new  Rack::MockRequest.env_for( path, {:method => method, :params=>params} )
+  end
+
+
+  def test_01_requests_serialize_and_reconstitute_back_and_forth
+
+    r0 = new_ml_request('POST', '/ignored',{ "Add"=>"Add", "MuffinContents"=>"chickens" })
+    s0 = r0.serialized
+
+    r1 = Ml_RackRequest::reconstitute_from( s0 )
+    s1 = r1.serialized
+
+    s0.should == s1
+  end
+
+
+
+  def test_02_reading_params_changes_serialization
+
+    r0 = new_ml_request('POST', '/ignored',{ "Add"=>"Add", "MuffinContents"=>"more chickens" })
+    s0 = r0.serialized
+
+    p r0.incoming_contents
+    whatever = r0.incoming_contents
+
+    s1 = r0.serialized
+    s1.should_not == s0
+
+  end
+
+
+  def test_03_forcing_params_read_solves_serialization_problem
+
+    r0 = new_ml_request('POST', '/ignored',{ "Add"=>"Add", "MuffinContents"=>"less chickens" })
+
+    p r0.incoming_contents
+
+    s0 = r0.serialized
+
+    whatever = r0.incoming_contents
+
+    s1 = r0.serialized
+    s1.should == s0
+
+  end
+
+
+end
+
